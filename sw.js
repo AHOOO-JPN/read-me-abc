95e65721078b655f25eafed32f82d560e00562b9
AHOOO-JPN <soulmate1209@gmail.com>
2025-09-29 13:59:40 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,42 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = "readmeabc-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./read_me_icon_192.png",
+  "./read_me_icon_512.png",
+  "./style.css",   // あれば
+  "./script.js"    // あれば
+];
+
+// インストール時にキャッシュ
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open('readmeabc-v1').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './read_me_icon_192.png',
-        './read_me_icon_512.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
+// 有効化時に古いキャッシュ削除
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// フェッチ時にキャッシュ優先
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
       return response || fetch(e.request);
